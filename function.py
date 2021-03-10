3fe67003aeda5edf4798cb355a8e4af0a6ffbc0e
jum648 <78852579+jum648@users.noreply.github.com>
2021-03-10 14:57:00 +0300

    Add files via upload

---
+++ function.py
@@ -0,0 +1,36 @@
+def my_function():
+  print("hello world")
+my_function()
+
+def sum(x,y):
+  a=x+y
+  print("The sum is",a)
+sum(10,20)
+sum(20,20)
+def product(x,y):
+  a=x*y
+  print("The product is",a)
+product(10,40)
+product(20,50)
+
+def sum(x,y):
+  a=x+y
+  return a
+print(sum(10,20,26))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
